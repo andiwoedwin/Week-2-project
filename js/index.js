d6fbf903f37d834c13b5537f652d61ff254899e4
andiwoedwin <andiwoedwin@gmail.com>
2020-07-18 10:41:13 +0300

    add function

--- js/index.js
+++ js/index.js
@@ -2,12 +2,16 @@ var Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 
-function day() {
-    var d= parseInt(document.getElementById("DD").value);
-    var month = parseInt(document.getElementById("MM").value);
-    var year = parseInt(document.getElementById("YY").value);
-    var centuary = parseInt(year.slice(0, 2));
-    var YY = parseInt(year.slice(2, 4));
-
+function findingDayValue(){
+    year = document.getElementById("YY").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("MM").value);
+    DD = parseInt(document.getElementById("DD").value);
+    d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+    console.log(d);
+    return (Math.floor(d))
     alert("d")
+    
 }
+function ()
