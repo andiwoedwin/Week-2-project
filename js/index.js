a1f0c1aa9b3d0ebb6ee79414ce1459d2db9d27c5
andiwoedwin <andiwoedwin@gmail.com>
2020-07-18 22:54:20 +0300

    add function

--- js/index.js
+++ js/index.js
@@ -2,12 +2,24 @@ var daysOfWeek = ["sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Frida
 var male = ["Kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
 var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 function day(){
-    var d = parseInt(document.getElementById("d").value);
-    var m = parseInt(document.getElementById("m").value);
-    var y = parseInt(document.getElementById("y").value);
-    var CC = parseInt(year.slice(0, 2));
-    var yy = parseInt(year.slice(2, 4));
+    var d = document.getElementById("d").value;
+    var m = document.getElementById("m").value;
+    var y = document.getElementById("yy").value;
+    var CC =year.slice(0, 2));
+    var yy =year.slice(2, 4));
+    var gender =document.getElementById("y").value;
+    
+    
 
-    (d) = ( ( (CC/4) -2*CC-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d ) %7
+    (d) = ( ( (CC/4) -2*CC-1) + ((5*yy/4) ) + ((26*(m+1)/10)) + d ) %7
     alert(d)
+    if(d<1 || d>31){
+        alert("invalid date");
+    }
+    if(m<1 || m>12){
+
+    }
+}
+function getName(){
+    if()
 }
