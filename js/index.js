dcae68d8740f4516790738495eb0f42267ce7836
andiwoedwin <andiwoedwin@gmail.com>
2020-07-20 00:36:56 +0300

    add formula to our js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,69 @@
-var daysOfWeek = ["sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "Saturday"];
-var male = ["Kwasi", "kwado", "kwabena", "kwaku", "Yaw", "Kofi", "Kwame"];
-var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-function day(){
-    var d = document.getElementById("d").value;
-    var m = document.getElementById("m").value;
-    var y = document.getElementById("yy").value;
-    var CC =year.slice(0, 2));
-    var yy =year.slice(2, 4));
-    var gender =document.getElementById("y").value;
-    
-    
-
-    (d) = ( ( (CC/4) -2*CC-1) + ((5*yy/4) ) + ((26*(m+1)/10)) + d ) %7
-    alert(d)
-    if(d<1 || d>31){
-        alert("invalid date");
-    }
-    if(m<1 || m>12){
-
-    }
-}
-function getName(){
-    if()
-}
+function getDay(){
+       event.preventDefault();
+       var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                       //  0         1           2           3           4           5           6
+       var maleAkanName = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+                       //      0       1           2       3       4       5       6
+       var femaleAkanName = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+                       //       0         1       2       3       4       5       6
+       var day = parseInt(document.getElementById("dd").value);
+       var month = parseInt(document.getElementById("mm").value);
+       var year = document.getElementById("yy").value;
+       var gender = document.getElementById("gender").value;
+       var date = new Date(year +"/" + month + "/"+day);
+       var actualBirthDay = date.getDay();
+       var akanDay = dayOfWeek[actualBirthDay];
+       var invalidMonth = ( month<1 || month>12 || ((document.getElementById("mm").value)=="") );
+       var invalidDay = ( day<1 || day>31 || ((document.getElementById("dd").value)=="") );
+       var invalidYear = (  year<1 || ( year>( new Date().getFullYear() ) ) || ((document.getElementById("yy").value)=="") );
+       var invalidGender = ( (gender=="selectdefault") );
+       var invalidAll = ((invalidDay==true)&&(invalidMonth==true)&&(invalidYear==true)&&(invalidGender==true));
+       var validFemale = ((invalidDay==false)&&(invalidMonth==false)&&(invalidYear==false)&&(gender==="Female"));
+       var validMale = ((invalidDay==false)&&(invalidMonth==false)&&(invalidYear==false)&&(gender==="Male"));
+       var febDates = ( ((month===2)&&(day==30)) || ((month===2)&&(day==31)) );
+       var aprJune =  ( ((month===4)&&(day==31)) || ((month===6)&&(day==31)) );
+       var septOct =  ( ((month===9)&&(day==31)) || ((month===11)&&(day==31)) );
+       if ( invalidAll==true ) {
+        document.getElementById('result').textContent = "*ENTER DETAILS!";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("resultintro").style.display = "none";
+       }
+      else if( invalidDay==true) {
+        document.getElementById("result").textContent = "*Please enter a Valid Day Number from 1 - 31";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("results").style.display = "none";
+      }
+      else if ( invalidMonth==true ) {
+        document.getElementById('result').textContent = "*Please enter a Valid Month Number from 1 - 12";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("results").style.display = "none";
+       }
+      else if ( invalidYear==true ) {
+        document.getElementById('result').textContent = "*INVALID YEAR";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("results").style.display = "none";
+       }
+      else if ( invalidGender==true ) {
+        document.getElementById('result').textContent = "*SELECT GENDER";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("results").style.display = "none";
+        }
+      else if ( (febDates==true) || (aprJune==true) ||(septOct==true) ) {
+        document.getElementById('result').textContent = "*SELECT AN EXISTING DATE.";
+        document.getElementById("result").style.color = "red";
+        document.getElementById("results").style.display = "none";
+        }
+      else if ( validFemale==true ) {
+        document.getElementById('result').textContent = "You were born on "+ akanDay+ " and your Akan name is " +femaleAkanName[actualBirthDay];
+        document.getElementById("result").style.color = "green";
+        document.getElementById("results").style.display = "none";
+       }
+      else if ( validMale==true) {
+        document.getElementById('result').textContent = "You were born on "+ akanDay+ " and your Akan name is " +maleAkanName[actualBirthDay];
+        document.getElementById("result").style.color = "green";
+        document.getElementById("results").style.display = "none";
+       }
+      else {
+        alert("Dont joke");
+       }
+     }
